3dacaa7e8e36f0b1aac0e2b643d36aa25a813a4a
mycatisadog <paultang1999@gmail.com>
2019-11-21 10:25:29 -0800

    Add files via upload

--- testgenerate_map_html_file.R
+++ testgenerate_map_html_file.R
@@ -1,6 +1,7 @@
 list.of.packages <- c("leaflet", "htmlwidgets")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
+if(length(new.packages))
+  install.packages(new.packages)
 
 
 # Start Script Here: ------------------------------------------------------
@@ -10,16 +11,16 @@ library(leaflet)
 my.path = 'paulassignment.csv'
 
 dataf = read.csv(my.path, stringsAsFactors = F)
-dataf$Latitude = as.numeric(df$Latitude)
+dataf$Latitude = as.numeric(dataf$Latitude)
 
 popup <- paste("<div class='leaflet-popup-scrolled' style='max-width:600px;max-height:300px'><b>",
-               '<a href=', df$InfoURL, ' target="_blank"> <h2 align="center"><strong>', df$Name, "</strong></h2></a><br>",
-               "</b>", df$Description, '<br><br><center><img src="', df$ImageURL, '" height="150px" width="200px" border="8"></center><br> </div>')
+               '<a href=', dataf$InfoURL, ' target="_blank"> <h2 align="center"><strong>', dataf$Name, "</strong></h2></a><br>",
+               "</b>", dataf$Description, '<br><br><center><img src="', dataf$ImageURL, '" height="150px" width="200px" border="8"></center><br> </div>')
 
 
 map = leaflet() %>%
     addTiles( ) %>%
-    addMarkers(data = df, lat = df$Latitude, lng = df$Longitude, popup = popup )
+    addMarkers(data = dataf, lat = dataf$Latitude, lng = dataf$Longitude, popup = popup )
 
 map
 
